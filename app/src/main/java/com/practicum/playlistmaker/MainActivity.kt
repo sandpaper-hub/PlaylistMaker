dcc08733f84c26eee55aa5b514e2f7d93bc5ed56
Constantine Kim <electro.passion92@gmail.com>
2023-11-26 22:38:33 +0900

    Init

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -2,10 +2,37 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val searchButtonListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Search Button", Toast.LENGTH_SHORT).show()
+            }
+        }
+        searchButton.setOnClickListener(searchButtonListener)
+        val mediaLibraryButton =
+            findViewById<Button>(R.id.media_library_button).setOnClickListener {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Media Library Button",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+        val settingsButtonListener = View.OnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Settings Button",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        settingsButton.setOnClickListener(settingsButtonListener)
     }
 }
