0774e598aa92b5f70ca40ae2d614be316b521766
Constantine Kim <electro.passion92@gmail.com>
2023-11-27 00:40:57 +0900

    Change onClickListner's

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,26 +13,19 @@ class MainActivity : AppCompatActivity() {
         val searchButton = findViewById<Button>(R.id.search_button)
         val searchButtonListener = object : View.OnClickListener {
             override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Search Button", Toast.LENGTH_SHORT).show()
+                val searchActivityIntent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(searchActivityIntent)
             }
         }
         searchButton.setOnClickListener(searchButtonListener)
         val mediaLibraryButton =
             findViewById<Button>(R.id.media_library_button).setOnClickListener {
-                Toast.makeText(
-                    this@MainActivity,
-                    "Media Library Button",
-                    Toast.LENGTH_SHORT
-                ).show()
+                val mediaLibraryActivityIntent = Intent(this, MediaLibraryActivity::class.java)
+                startActivity(mediaLibraryActivityIntent)
             }
-        val settingsButton = findViewById<Button>(R.id.settings_button)
-        val settingsButtonListener = View.OnClickListener {
-            Toast.makeText(
-                this@MainActivity,
-                "Settings Button",
-                Toast.LENGTH_SHORT
-            ).show()
+        val settingsButton = findViewById<Button>(R.id.settings_button).setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
         }
-        settingsButton.setOnClickListener(settingsButtonListener)
     }
 }
