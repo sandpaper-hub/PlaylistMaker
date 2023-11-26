8fb80cc2ca812e56811f106dc24c6d506cc895f7
Constantine Kim <electro.passion92@gmail.com>
2023-11-27 00:42:43 +0900

    Add listener to back button

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,21 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val backButton = findViewById<ImageButton>(R.id.back_image_view).setOnClickListener{
+            val backIntent = Intent(this, MainActivity::class.java)
+            backIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            backIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            startActivity(backIntent)
+        }
+
     }
 }
