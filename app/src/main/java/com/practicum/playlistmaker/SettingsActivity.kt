33d90c90e727cabc51b18cc43ed76769ed5d3558
Constantine Kim <electro.passion92@gmail.com>
2023-12-24 23:08:23 +0900

    Revert "Sprint #9"

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,48 +1,21 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageButton
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        val message = getString(R.string.sampleMessageForShare)
-        val url = getString(R.string.privacyUrl)
-        val email = getString(R.string.sampleEmail)
-        val subject = getString(R.string.sampleSubject)
-        val body = getString(R.string.sampleBodyMessage)
 
-        val backButtonListener =
-            findViewById<ImageButton>(R.id.back_button_settingsActivity).setOnClickListener {
-                val backIntent = Intent(this, MainActivity::class.java)
-                backIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-                backIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
-                startActivity(backIntent)
-            }
-
-        val shareButtonListener = findViewById<ImageView>(R.id.shareImageView).setOnClickListener {
-            val shareIntent = Intent(Intent.ACTION_SEND)
-            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
-            shareIntent.setType("text/plain")
-            val intentChooser = Intent.createChooser(shareIntent, " ")
-            startActivity(intentChooser)
+        val backButton = findViewById<ImageButton>(R.id.back_image_view).setOnClickListener{
+            val backIntent = Intent(this, MainActivity::class.java)
+            backIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            backIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            startActivity(backIntent)
         }
 
-        val supportButtonListener =
-            findViewById<ImageView>(R.id.supportImageView).setOnClickListener {
-                val supportIntent = Intent(Intent.ACTION_SENDTO)
-                supportIntent.setData(Uri.parse("mailto:$email?subject=$subject&body=$body"))
-                startActivity(supportIntent)
-            }
-
-        val privacyButtonListener = findViewById<ImageView>(R.id.privacyImageView).setOnClickListener {
-            val privacyIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-            startActivity(privacyIntent)
-        }
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++
@@ -1,8 +0,0 @@
-package com.practicum.playlistmaker
-
-data class Track(
-    val trackName: String,
-    val artistName: String,
-    val trackDuration: String,
-    val artworkUrl100: String
-)
