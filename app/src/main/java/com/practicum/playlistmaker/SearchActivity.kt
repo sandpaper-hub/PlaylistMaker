d327d1fa024581c7650a34ab4a8a9c9d14390bd2
Constantine Kim <electro.passion92@gmail.com>
2023-12-25 09:48:07 +0900

    Init

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -12,16 +12,17 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.core.widget.doOnTextChanged
+import androidx.recyclerview.widget.RecyclerView
 
 class SearchActivity : AppCompatActivity() {
 
     var savedText = ""
     private var restoredText = ""
+    private val trackList: ArrayList<Track> = TrackListMockObject.listOfTrack
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-
 
         val backButton = findViewById<ImageButton>(R.id.back_button_searchActivity)
         backButton.setOnClickListener {
@@ -51,13 +52,17 @@ class SearchActivity : AppCompatActivity() {
             }
         }
 
+        val trackListRecyclerView = findViewById<RecyclerView>(R.id.trackListRecyclerView)
+        val trackListAdapter = TrackListAdapter(trackList)
+        trackListRecyclerView.adapter = trackListAdapter
+
         searchEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                //not yet implemented
+            //not yet implemented
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                //not yet implemented
+            //not yet implemented
             }
 
             override fun afterTextChanged(s: Editable?) {
---
+++ app/src/main/java/com/practicum/playlistmaker/TrackListAdapter.kt
@@ -0,0 +1,24 @@
+package com.practicum.playlistmaker
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TrackListAdapter(private val trackList: List<Track>) :
+    RecyclerView.Adapter<TrackViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        return TrackViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.track_list, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return trackList.size
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        Log.d("ADAPTER", "HERE")
+        holder.bind(trackList[position])
+    }
+}
