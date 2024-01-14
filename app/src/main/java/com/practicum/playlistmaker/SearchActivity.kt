f196ceb7dfbbdc9453ddcc6e861fe4ddc1c2945d
Constantine Kim <electro.passion92@gmail.com>
2024-01-14 18:41:55 +0900

    Refactor Add baseUrl

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -8,23 +8,39 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
 
     var savedText = ""
     private var restoredText = ""
-    private val trackList: ArrayList<Track> = TrackListMockObject().listOfTrack
+    private val baseUrl = "https://itunes.apple.com"
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    private val trackList: ArrayList<Track> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val backButton = findViewById<ImageButton>(R.id.back_button_searchActivity)
+        val searchEditText = findViewById<EditText>(R.id.search_editText)
+        val clearSearchButton = findViewById<ImageButton>(R.id.clear_search_ediText)
+        val trackListRecyclerView = findViewById<RecyclerView>(R.id.trackListRecyclerView)
+        val trackListAdapter = TrackListAdapter(trackList)
+
+        trackListRecyclerView.adapter = trackListAdapter
+
         backButton.setOnClickListener {
             val backIntent = Intent(this, MainActivity::class.java)
             backIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
@@ -32,18 +48,7 @@ class SearchActivity : AppCompatActivity() {
             startActivity(backIntent)
         }
 
-        val searchEditText = findViewById<EditText>(R.id.search_editText)
         searchEditText.isSaveEnabled = false
-        val clearSearchButton = findViewById<ImageButton>(R.id.clear_search_ediText)
-        clearSearchButton.visibility = GONE
-
-        clearSearchButton.setOnClickListener {
-            searchEditText.setText("")
-            val inputMethodManager =
-                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-            inputMethodManager?.hideSoftInputFromWindow(it.windowToken, 0)
-        }
-
         searchEditText.doOnTextChanged { text, _, _, _ ->
             if (text.isNullOrEmpty()) {
                 clearSearchButton.visibility = GONE
@@ -52,17 +57,13 @@ class SearchActivity : AppCompatActivity() {
             }
         }
 
-        val trackListRecyclerView = findViewById<RecyclerView>(R.id.trackListRecyclerView)
-        val trackListAdapter = TrackListAdapter(trackList)
-        trackListRecyclerView.adapter = trackListAdapter
-
         searchEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            //TODO  not yet implemented
+                //TODO  not yet implemented
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            //TODO not yet implemented
+                //TODO not yet implemented
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -70,6 +71,23 @@ class SearchActivity : AppCompatActivity() {
             }
 
         })
+
+        searchEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                //TODO запрос
+                true
+            }
+            false
+        }
+
+
+        clearSearchButton.visibility = GONE
+        clearSearchButton.setOnClickListener {
+            searchEditText.setText("")
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(it.windowToken, 0)
+        }
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
