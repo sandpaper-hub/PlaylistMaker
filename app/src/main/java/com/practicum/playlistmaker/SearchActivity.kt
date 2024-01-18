29c98878eda69c6a5838427b9ecde4a0ba026548
Constantine Kim <electro.passion92@gmail.com>
2024-01-18 22:30:49 +0900

    Add ITunes API request

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,8 +1,8 @@
 package com.practicum.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -10,34 +10,62 @@ import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.RecyclerView
-import com.google.gson.Gson
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
 
+    companion object {
+        const val INSTANCE_STATE_KEY = "SAVED_RESULT"
+    }
+
     var savedText = ""
     private var restoredText = ""
+
     private val baseUrl = "https://itunes.apple.com"
     private val retrofit = Retrofit.Builder()
         .baseUrl(baseUrl)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+    private val iTunesSearchService = retrofit.create(ITunesSearchApi::class.java)
+
     private val trackList: ArrayList<Track> = ArrayList()
 
+    private lateinit var badSearchResultImage: ImageView
+    private lateinit var badSearchResultTextView: TextView
+    private lateinit var refreshSearchButton: Button
+    lateinit var searchEditText: EditText
+    lateinit var trackListAdapter: TrackListAdapter
+
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val backButton = findViewById<ImageButton>(R.id.back_button_searchActivity)
-        val searchEditText = findViewById<EditText>(R.id.search_editText)
+        searchEditText = findViewById(R.id.search_editText)
         val clearSearchButton = findViewById<ImageButton>(R.id.clear_search_ediText)
         val trackListRecyclerView = findViewById<RecyclerView>(R.id.trackListRecyclerView)
-        val trackListAdapter = TrackListAdapter(trackList)
+
+        trackListAdapter = TrackListAdapter(trackList)
+        badSearchResultImage = findViewById(R.id.badSearchResultImage)
+        badSearchResultTextView = findViewById(R.id.badSearchResultText)
+        refreshSearchButton = findViewById(R.id.refresh_search_button)
+
+        badSearchResultImage.visibility = GONE
+        badSearchResultTextView.visibility = GONE
+        refreshSearchButton.visibility = GONE
 
         trackListRecyclerView.adapter = trackListAdapter
 
@@ -74,12 +102,10 @@ class SearchActivity : AppCompatActivity() {
 
         searchEditText.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                //TODO запрос
-                true
+                doRequest()
             }
             false
         }
-
 
         clearSearchButton.visibility = GONE
         clearSearchButton.setOnClickListener {
@@ -87,8 +113,17 @@ class SearchActivity : AppCompatActivity() {
             val inputMethodManager =
                 getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(it.windowToken, 0)
+            showBadResult("", 0)
+            trackList.clear()
+            trackListAdapter.notifyDataSetChanged()
+        }
+
+        refreshSearchButton.setOnClickListener {
+            doRequest()
         }
     }
+
+
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
@@ -102,7 +137,63 @@ class SearchActivity : AppCompatActivity() {
         outState.putString(INSTANCE_STATE_KEY, savedText)
     }
 
-    companion object {
-        const val INSTANCE_STATE_KEY = "SAVED_RESULT"
+    private fun doRequest() {
+        iTunesSearchService.search(searchEditText.text.toString()).enqueue(object :
+            Callback<TrackResponse> {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onResponse(
+                call: Call<TrackResponse>,
+                response: Response<TrackResponse>
+            ) {
+                if (response.code() == 200) {
+                    trackList.clear()
+                    if (response.body()?.results?.isNotEmpty() == true) {
+                        trackList.addAll(response.body()?.results!!)
+                        trackListAdapter.notifyDataSetChanged()
+                    }
+                    if (trackList.isEmpty()) {
+                        showBadResult(
+                            applicationContext.resources.getString(R.string.nothing_found),
+                            R.drawable.nothing_found_image
+                        )
+                    } else {
+                        showBadResult("", 0)
+                    }
+                } else {
+                    showBadResult(
+                        applicationContext.resources.getString(R.string.connection_error),
+                        R.drawable.bad_connection_image
+                    )
+                }
+            }
+
+            override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                showBadResult(
+                    applicationContext.resources.getString(R.string.connection_error),
+                    R.drawable.bad_connection_image
+                )
+            }
+        })
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun showBadResult(message: String, image: Int) {
+        if (message.isNotEmpty()) {
+            badSearchResultImage.visibility = VISIBLE
+            badSearchResultTextView.visibility = VISIBLE
+            badSearchResultTextView.text = message
+            badSearchResultImage.setImageResource(image)
+            if (message == applicationContext.resources.getString(R.string.connection_error)) {
+                refreshSearchButton.visibility = VISIBLE
+            } else {
+                refreshSearchButton.visibility = GONE
+            }
+            trackList.clear()
+            trackListAdapter.notifyDataSetChanged()
+        } else {
+            badSearchResultImage.visibility = GONE
+            badSearchResultTextView.visibility = GONE
+            refreshSearchButton.visibility = GONE
+        }
     }
 }
