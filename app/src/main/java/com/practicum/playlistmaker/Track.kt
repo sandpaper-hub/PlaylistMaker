40c73d690a154a7e2e7434982d758fb0171353ba
Constantine Kim <electro.passion92@gmail.com>
2024-01-18 22:31:25 +0900

    Add @Serialized annotation

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -1,8 +1,10 @@
 package com.practicum.playlistmaker
 
+import com.google.gson.annotations.SerializedName
+
 data class Track(
     val trackName: String,
     val artistName: String,
-    val trackDuration: String,
+    @SerializedName ("trackTimeMillis") val trackDuration: Long,
     val artworkUrl100: String
 )
