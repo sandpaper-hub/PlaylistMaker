43da924d00221bddc71ef9707857840e37cf9b67
Constantine Kim <electro.passion92@gmail.com>
2024-01-25 21:16:23 +0900

    Add SharedPreferencesData

---
+++ app/src/main/java/com/practicum/playlistmaker/SharedPreferencesData.kt
@@ -0,0 +1,8 @@
+package com.practicum.playlistmaker
+
+class SharedPreferencesData {
+    companion object {
+        const val sharedPreferencesFileName = "PlaylistMakerPreferences"
+        const val darkThemeKey = "isDarkTheme"
+    }
+}
