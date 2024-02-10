fcbc309b0398c14da3c50e0face36985b6c7d40f
Constantine Kim <electro.passion92@gmail.com>
2024-02-10 17:56:51 +0900

    Add data values

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -6,6 +6,10 @@ data class Track(
     val trackId: String,
     val trackName: String,
     val artistName: String,
-    @SerializedName ("trackTimeMillis") val trackDuration: Long,
-    val artworkUrl100: String
+    @SerializedName("trackTimeMillis") val trackDuration: Long,
+    val artworkUrl100: String,
+    val collectionName: String,
+    val releaseDate: String,
+    val primaryGenreName: String,
+    val country: String
 )
