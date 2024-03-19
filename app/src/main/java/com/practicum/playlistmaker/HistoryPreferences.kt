35618b14c04a3dbc6926d59c429530c642454196
Constantine Kim <electro.passion92@gmail.com>
2024-03-19 22:55:40 +0900

    Refactor imports

--- app/src/main/java/com/practicum/playlistmaker/HistoryPreferences.kt
+++ app/src/main/java/com/practicum/playlistmaker/HistoryPreferences.kt
@@ -2,6 +2,7 @@ package com.practicum.playlistmaker
 
 import android.content.SharedPreferences
 import com.google.gson.Gson
+import com.practicum.playlistmaker.domain.models.Track
 
 class HistoryPreferences(private val sharedPreferences: SharedPreferences) {
 
