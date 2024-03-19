bfbbc495853119735a0a1cebc96c092addd68dc2
Constantine Kim <electro.passion92@gmail.com>
2024-03-19 22:50:14 +0900

    Removed to data layer

--- app/src/main/java/com/practicum/playlistmaker/TrackResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.practicum.playlistmaker
-
-class TrackResponse (
-    val results: List<Track>
-)
---
+++ app/src/main/java/com/practicum/playlistmaker/data/dto/TrackResponse.kt
@@ -0,0 +1,7 @@
+package com.practicum.playlistmaker.data.dto
+
+import com.practicum.playlistmaker.domain.models.Track
+
+class TrackResponse (
+    val results: List<Track>
+)
