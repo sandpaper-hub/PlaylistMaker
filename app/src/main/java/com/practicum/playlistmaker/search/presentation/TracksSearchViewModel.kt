fb22cde29697cc77d173ee758920bd618c413e0b
Constantine Kim <electro.passion92@gmail.com>
2024-06-12 17:56:56 +0900

    Refactor unused import

--- app/src/main/java/com/practicum/playlistmaker/search/presentation/TracksSearchViewModel.kt
+++ app/src/main/java/com/practicum/playlistmaker/search/presentation/TracksSearchViewModel.kt
@@ -1,6 +1,5 @@
 package com.practicum.playlistmaker.search.presentation
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
