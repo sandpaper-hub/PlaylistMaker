613cbbf289ca3fe0090dc3870d609ac7ca6cf27c
Constantine Kim <electro.passion92@gmail.com>
2024-07-01 23:54:23 +0900

    Remove unused imports

--- app/src/main/java/com/practicum/playlistmaker/mediaLibrary/presentation/favorite/FavoriteTracksViewModel.kt
+++ app/src/main/java/com/practicum/playlistmaker/mediaLibrary/presentation/favorite/FavoriteTracksViewModel.kt
@@ -1,6 +1,5 @@
 package com.practicum.playlistmaker.mediaLibrary.presentation.favorite
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
