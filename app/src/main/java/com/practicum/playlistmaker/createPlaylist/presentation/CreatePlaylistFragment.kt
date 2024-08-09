d84c0ad0c70cbc5897e9f178a38ef7f961d7eab0
Constantine Kim <electro.passion92@gmail.com>
2024-08-10 05:59:14 +0900

    Create new fragment (CreatePlaylistFragment)

---
+++ app/src/main/java/com/practicum/playlistmaker/createPlaylist/presentation/CreatePlaylistFragment.kt
@@ -0,0 +1,26 @@
+package com.practicum.playlistmaker.createPlaylist.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.practicum.playlistmaker.databinding.FragmentCreatePlaylistBinding
+
+class CreatePlaylistFragment : Fragment() {
+    private lateinit var binding: FragmentCreatePlaylistBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCreatePlaylistBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.createButton.isEnabled = false
+    }
+}
