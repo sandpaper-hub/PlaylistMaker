fcd75ae9f21a8714a7d8d41cc27571da83a2d7c5
Constantine Kim <electro.passion92@gmail.com>
2024-08-13 05:46:24 +0900

    Add TextWatcher Changed button disabled

--- app/src/main/java/com/practicum/playlistmaker/createPlaylist/presentation/CreatePlaylistFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/createPlaylist/presentation/CreatePlaylistFragment.kt
@@ -1,6 +1,9 @@
 package com.practicum.playlistmaker.createPlaylist.presentation
 
 import android.os.Bundle
+import android.os.TokenWatcher
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +13,7 @@ import com.practicum.playlistmaker.databinding.FragmentCreatePlaylistBinding
 
 class CreatePlaylistFragment : Fragment() {
     private lateinit var binding: FragmentCreatePlaylistBinding
+    private lateinit var textWatcher: TextWatcher
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,5 +26,22 @@ class CreatePlaylistFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        textWatcher = object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val string = s?.toString()
+                binding.createButton.isEnabled = string?.isNotEmpty() ?: false
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        }
+
+        textWatcher.let { binding.albumNameEditText.addTextChangedListener(it) }
     }
 }
